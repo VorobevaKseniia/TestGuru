277daa062d133bd0f0b8dab510eb7004493e73ef
Kseniia <ilijamarmelad@gmail.com>
2023-10-23 16:56:41 +0200

    fix: instatnce method list_of_tests and class method sort_by_categor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def list_of_tests(level, id)
+    Test.joins("INNER JOIN user_tests ON tests.id = user_tests.test_id").where(tests: {level: level})
+        .where(user_tests: {id: id}).pluck(:title)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CATEGORIES title
+categories = Category.create!([
+                                {title: 'HTML'},
+                                {title: 'Ruby'},
+                                {title: 'SQL'}
+                              ])
+
+# TESTS title, level
+tests = Test.create!([
+                       {title: 'Basics of HTML', level: 1, category_id: 1},
+                       {title: 'Basics of Ruby', level: 2, category_id: 2},
+                       {title: 'Basics of SQL', level: 2, category_id: 3}
+                     ])
+
+# USERS name, status
+users = User.create!([
+                       {name: 'Ivan', status: :user},
+                       {name: 'Elene', status: :admin},
+                       {name: 'Kate', status: :user}
+                     ])
+# USER_TESTS
+us_tes = UserTest.create!(user_id: 1, test_id: 1)
+us_tes2 = UserTest.create!(user_id: 1, test_id: 2)
+
+# QUESTIONS body
+questions = Question.create!([
+                               {body: 'What tag is used to move to a new line?', test_id: 1},
+                               {body: 'What does the attribute "href" contain?', test_id: 1},
+
+                               {body: 'Which method allows to translate a string to lower case?', test_id: 2},
+                               {body: 'What is the name of the most popular Ruby framework for the web?', test_id: 2},
+
+                               {body: 'What query can be used to retrieve a list of all databases?', test_id: 3},
+                               {body: 'Which keyword is used to cancel a transaction?', test_id: 3}
+                             ])
+
+# ANSWERS body, correct
+answers = Answer.create!([
+                           {body: '<br/>', question_id: 1},
+                           {body: 'URL of the page to which the redirection will take place', question_id: 2},
+
+                           {body: 'downcase()', question_id: 3},
+                           {body: 'Rails', question_id: 4},
+
+                           {body: 'SHOW DATABASES', question_id: 5},
+                           {body: 'ROLLBACK', question_id: 6}
+                         ])
