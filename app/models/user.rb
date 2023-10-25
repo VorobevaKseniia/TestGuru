7f97a5f7eeae8a5ec3b729144b3f7c40c840e5dc
Kseniia <ilijamarmelad@gmail.com>
2023-10-25 17:34:05 +0200

    feat: model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :user_tests
+  has_many :tests, through: :user_tests
   def list_of_tests(level)
     Test.joins("INNER JOIN user_tests ON tests.id = user_tests.test_id").where(tests: {level: level})
         .where(user_tests: {user_id: id}).pluck(:title)
