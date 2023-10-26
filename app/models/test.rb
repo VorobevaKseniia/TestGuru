c414c29380690489786eda22ef81597c7cf32fa6
Kseniia <ilijamarmelad@gmail.com>
2023-10-26 16:56:59 +0200

    refactor: instans/ class methods feat: relationship Test-admin

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :user, -> { where status: :admin}
   has_many :questions
   has_many :user_tests
   has_many :users, through: :user_tests
+
+
   def self.sort_by_category(category)
-    Test.joins("INNER JOIN categories ON categories.id = tests.category_id")
-        .where(categories: { title: category }).order(title: :desc).pluck(:title)
+    joins(:category).where(category: { title: category }).order(title: :desc).pluck(:title)
   end
+
 end
