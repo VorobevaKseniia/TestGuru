762ed46f463964837f18ca48ebdec201850a9653
Kseniia <ilijamarmelad@gmail.com>
2023-10-30 12:16:59 +0100

    fix: compound index per field bundle

--- db/migrate/20231029163324_add_index_to_title_in_tests.rb
+++ db/migrate/20231029163324_add_index_to_title_in_tests.rb
@@ -1,6 +1,5 @@
 class AddIndexToTitleInTests < ActiveRecord::Migration[6.1]
   def change
-    add_index :tests, :title, unique: true
-    # add_index :tests, :level, unique: true
+    add_index :tests, [:title, :level], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2023_10_29_163324) do
     t.integer "author_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["title"], name: "index_tests_on_title", unique: true
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "user_tests", force: :cascade do |t|
