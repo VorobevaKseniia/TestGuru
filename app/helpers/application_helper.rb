9df0c3bd9ec12c44d6def7abc9ceb50542b5cc17
Kseniia <ilijamarmelad@gmail.com>
2023-11-27 23:08:59 +0100

    Styling using Bootstrap framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank'
+    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank',
+            class: "fw-bolder link-secondary text-decoration-none"
   end
 end
