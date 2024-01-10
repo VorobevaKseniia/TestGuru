5d123ab1c9368767abf95666a348c52fd922a94e
Kseniia <ilijamarmelad@gmail.com>
2024-01-10 18:28:44 +0100

    refactor: gist/flash

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -5,24 +5,16 @@ class GistsController < ApplicationController
     service = GistQuestionService.new(@test_passage.current_question)
     result = service.call
     @flash_options ||= if service.success?
-                      create_gist(result.html_url)
+                         Gist.create(
+                           question_id: @test_passage.current_question.id,
+                           user_id: current_user.id,
+                           gist_url: result.html_url
+                         )
                       {notice: t('.success', link: result.html_url)}
                     else
                       {alert: t('.failure')}
                     end
     redirect_to @test_passage, @flash_options
 
-  end
-
-  private
-
-  def create_gist(url)
-    Gist.create!(
-      question_id: @test_passage.current_question.id,
-      user_id: current_user.id,
-      gist_url: url
-    )
-  rescue StandardError => e
-    @flash_options = {alert: "#{e.message}"}
   end
 end
