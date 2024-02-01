278cc2ab6a676744ab5c08319561e36254c74ba3
Kseniia <ilijamarmelad@gmail.com>
2024-02-01 14:55:12 +0100

    feat: add sort by header for tables; js password validation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../utilities/sorting'
+import '../utilities/check-password'
 
 Rails.start()
 Turbolinks.start()
